85e57407029b04a46e32cd7721c87e929e466a9f
Evan Campbell <hydrogenmeerkat@hotmail.com>
2009-04-08 19:44:34 -0700

    the program should spit out a 5 letter word backwards. input hello output olleh.

---
+++ secondprogram.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<stdio.h>
+
+main()
+{
+char ch1,ch2,ch3,ch4,ch5;
+
+printf("type 5 letter word\n");
+scanf ("%c,%c,%c,%c,%c\n",ch1,ch2,ch3,ch4,ch5);
+printf("%c,%c,%c,%c,%c\n",ch5,ch4,ch3,ch2,ch1);
+}
