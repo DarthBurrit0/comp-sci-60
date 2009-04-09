9f8fbb91bc8f06e41b623a8db824f5cbd3abf29f
Jason Campbell <jason@wearegreatergood.com>
2009-04-08 19:59:56 -0700

    A tiny bit of help

--- secondprogram.c
+++ secondprogram.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include<stdio.h>
 
 main()
 {
 char ch1,ch2,ch3,ch4,ch5;
 
 printf("type 5 letter word\n");
-scanf ("%c,%c,%c,%c,%c\n",ch1,ch2,ch3,ch4,ch5);
+scanf("%c,%c,%c,%c,%c\n",&ch1,&ch2,&ch3,&ch4,&ch5);
 printf("%c,%c,%c,%c,%c\n",ch5,ch4,ch3,ch2,ch1);
 }
