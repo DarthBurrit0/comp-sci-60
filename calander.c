599d17a551d8ddeadb13cffacc2c26085c06c8d3
Evan Campbell <hydrogenmeerkat@hotmail.com>
2009-04-18 15:55:12 -0700

    cleaned up and wiki table

--- calander.c
+++ calander.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 main ()
 {
-  char calander;
-  char S,M,T,W,R,F;
-  int year;
-  /*user input*/
+  int month_number;
+ 
+  /*enter month number (number 1-12)*/
+ 
   printf("Enter something:\n");
-  scanf("%c %d",&calander,&year);
+  scanf("%d",&month_under);
   
   /*processing*/
+January = 0
+February = 3
+March = 3
+April = 6
+May = 1
+June = 4
+July = 6
+August = 2
+September = 5
+October = 0
+November = 3
+December = 5
+
+//table for day of the week calculation acquired from:
+// / http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week
   
   /*output*/
   printf("S\tM\tT\tW\tR\tF\tS\n");
