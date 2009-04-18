0a8b67a65ca763d213e9f47126ddd84766fd7757
Evan Campbell <hydrogenmeerkat@hotmail.com>
2009-04-18 16:21:33 -0700

    months_table_number is always coming back as 5

--- calander.c
+++ calander.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 main ()
 {
-  int month_number;
- 
-  /*enter month number (number 1-12)*/
- 
-  printf("Enter something:\n");
-  scanf("%d",&month_under);
-  
-  /*processing*/
-January = 0
-February = 3
-March = 3
-April = 6
-May = 1
-June = 4
-July = 6
-August = 2
-September = 5
-October = 0
-November = 3
-December = 5
+int month_number;
+int months_table_number;
 
+/*enter month number (number 1-12)*/
+printf("Type month number\n");
+scanf("%d",&month_number);
+
+/*processing*/
+/**
+* Processing the number of the week this month starts on, formula is
+* found on: http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week
+*/
+switch(month_number)
+  {
+case 1: months_table_number = 0;
+case 2: months_table_number = 3;
+case 3: months_table_number = 3;
+case 4: months_table_number = 6;
+case 5: months_table_number = 1;
+case 6: months_table_number = 4;
+case 7: months_table_number = 6;
+case 8: months_table_number = 2;
+case 9: months_table_number = 5;
+case 10: months_table_number = 0;
+case 11: months_table_number = 3;
+case 12: months_table_number = 5;
+  }
+ 
 //table for day of the week calculation acquired from:
 // / http://en.wikipedia.org/wiki/Calculating_the_day_of_the_week
-  
-  /*output*/
-  printf("S\tM\tT\tW\tR\tF\tS\n");
+
+/*output*/
+
+printf("%d\n",months_table_number);
+//printf("S\tM\tT\tW\tR\tF\tS\n");
 }
